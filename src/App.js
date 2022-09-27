785ba2a47462097c4d17cc9377bb4b450c93f018
JTDoe <Jordendoerflinger@gmail.com>
2022-09-27 09:00:07 -0500

    tables added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { ActivePlayers } from "./ActivePlayers";
+import { Bench } from './Bench'
 
 function App() {
-  return <div className="App">Hello react</div>;
+  return (
+<div>
+  <ActivePlayers />
+  <br></br>
+  <br></br>
+  <Bench />
+  </div>
+  )
 }
 
 export default App;
