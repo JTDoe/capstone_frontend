429f65cbabb6174abc39e85cc2ec0a1b3da7f9c9
JTDoe <Jordendoerflinger@gmail.com>
2022-09-29 22:43:38 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import "./App.css";
 // import { useState, useEffect } from "react";
-import { ActivePlayers } from "./ActivePlayers";
-import { Bench } from './Bench'
-import { BrowserRouter } from "react-router-dom";
+import { ActivePlayers } from "./components/ActivePlayers"
+import { Bench } from "./components/Bench"
+import { AddPlayer } from "./components/AddPlayer"
+import { AddPlayerButton } from "./components/AddPlayerButton"
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-<div>
-  <h2>Active Players</h2>
-  <ActivePlayers />
-  <br></br>
-  <br></br>
-  <h2>Bench</h2>
-  <Bench />
-  </div>
-  </BrowserRouter>
+    <>
+<AddPlayer /> <AddPlayerButton />
+<ActivePlayers />
+<Bench />
+</>
   )
 }
 
